ff3e66cbeb0968b49a1baed777350d62b460526e
Josephchinedu <joseph@digitaldesign247.com>
2020-04-17 14:18:54 +0100

    Updates

---
+++ precache-manifest.d664c7fba1da67f92f648f07d428b44e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034a765fb275a34eab2188c15d869f25",
+    "url": "/react-Calculator/index.html"
+  },
+  {
+    "revision": "4ee2cf718aaa0e8d7b93",
+    "url": "/react-Calculator/static/css/main.2dc0d1a9.chunk.css"
+  },
+  {
+    "revision": "d13c1eb9937cedbd6978",
+    "url": "/react-Calculator/static/js/2.772d3c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Calculator/static/js/2.772d3c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee2cf718aaa0e8d7b93",
+    "url": "/react-Calculator/static/js/main.2cf15adb.chunk.js"
+  },
+  {
+    "revision": "df36d2b13dc879d20c73",
+    "url": "/react-Calculator/static/js/runtime-main.ac367e3b.js"
+  }
+]);
